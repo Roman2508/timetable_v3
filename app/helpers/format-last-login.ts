8da7d9d38d7e2dc16264480a8ec9eb12868b1e6a
Roman2508 <2508romaa@gmail.com>
2025-07-02 19:23:25 +0300

    fix

--- app/helpers/format-last-login.ts
+++ app/helpers/format-last-login.ts
@@ -1,8 +1,8 @@
-import { dayjs } from "../lib/dayjs";
+import { customDayjs } from "../lib/dayjs";
 
 export const formatLastLogin = (date: string) => {
   if (!date) return "Ніколи";
   const cleanedDate = date.split(".")[0] + "Z";
-  const formatted = dayjs(cleanedDate).fromNow();
+  const formatted = customDayjs(cleanedDate).fromNow();
   return formatted;
 };
